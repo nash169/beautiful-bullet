132c0b8ce1e6f7251632a4ca6d9aac6814b34adc
bernardo <bernardo.fichera@gmail.com>
2021-03-14 14:06:42 +0100

    Added Magnum graphics wrapper.

---
+++ src/robot_bullet/graphics/AbstractGraphics.hpp
@@ -0,0 +1,37 @@
+
+#ifndef ROBOT_BULLET_GRAPHICS_ABSTRACTGRAPHICS_HPP
+#define ROBOT_BULLET_GRAPHICS_ABSTRACTGRAPHICS_HPP
+
+#include <cstddef>
+
+namespace robot_bullet {
+    class Simulator;
+
+    namespace graphics {
+        class AbstractGraphics {
+        public:
+            AbstractGraphics(double desiredFPS = 25) : _desired_fps(desiredFPS), _frame_counter(0), _init(false), _done(true), _pause(false) {}
+
+            virtual ~AbstractGraphics() {}
+
+            virtual void init(Simulator& sim) {}
+
+            virtual bool done() { return _done; }
+
+            virtual bool pause() { return _pause; }
+
+            virtual void refresh() {}
+
+            virtual void setDesiredFPS(size_t desiredFPS) { _desired_fps = desiredFPS; }
+
+            virtual void setEnable(bool) {}
+
+        protected:
+            bool _init, _done, _pause;
+
+            size_t _frame_counter, _render_period, _desired_fps;
+        };
+    } // namespace graphics
+} // namespace robot_bullet
+
+#endif // ROBOTRAISIM_GUI_ABSTRACTGRAPHICS_HPP
---
+++ src/robot_bullet/graphics/MagnumGraphics.hpp
@@ -0,0 +1,33 @@
+#ifndef ROBOT_BULLET_GRAPHICS_MAGNUM_GRAPHICS_HPP
+#define ROBOT_BULLET_GRAPHICS_MAGNUM_GRAPHICS_HPP
+
+#include <magnum_dynamics/MagnumApp.hpp>
+
+#include "robot_bullet/Simulator.hpp"
+#include "robot_bullet/graphics/AbstractGraphics.hpp"
+
+namespace robot_bullet {
+    namespace graphics {
+        using namespace magnum_dynamics;
+
+        class MagnumGraphics : public AbstractGraphics {
+        public:
+            MagnumGraphics(double desiredFPS = 25);
+
+            ~MagnumGraphics();
+
+            void init(Simulator& sim) override;
+
+            bool done() override;
+
+            bool pause() override;
+
+            void refresh() override;
+
+        protected:
+        };
+    } // namespace ogre
+} // namespace gui
+} // namespace robot_raisim
+
+#endif // ROBOT_BULLET_GRAPHICS_MAGNUM_GRAPHICS_HPP
